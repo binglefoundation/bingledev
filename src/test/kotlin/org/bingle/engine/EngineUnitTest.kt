4666603a48c2bb80459615e877cd654231e33306
Bingle Foundation <bingle.foundation@gmail.com>
2023-12-04 17:12:11 +1300

    - With tidy

--- src/test/kotlin/org/bingle/engine/EngineUnitTest.kt
+++ src/test/kotlin/org/bingle/engine/EngineUnitTest.kt
@@ -7,26 +7,21 @@ import io.mockk.verify
 import org.assertj.core.api.Assertions.assertThat
 import org.bingle.command.BaseCommand
 import org.bingle.command.Ping
-import org.bingle.command.ResponseCommand
-import org.bingle.command.TextMessageCommand
 import org.bingle.dtls.DTLSParameters
 import org.bingle.dtls.IDTLSConnect
 import org.bingle.dtls.NetworkSourceKey
 import org.bingle.engine.mocks.*
-import org.bingle.engine.mocks.endpoint2
-import org.bingle.engine.mocks.id2
-import org.bingle.util.logDebug
 import org.bingle.util.logWarn
 import org.junit.jupiter.api.Test
 import java.nio.charset.Charset
 import kotlin.reflect.jvm.javaMethod
 
 class EngineUnitTest {
-    val mockDtlsConnect = mockk<IDTLSConnect>()
-    val mockCommsConfig = MockCommsConfig(mockDtlsConnect)
-    lateinit var dtlsParameters: DTLSParameters
-    val id1nsk = NetworkSourceKey(endpoint1)
-    val id2nsk = NetworkSourceKey(endpoint2)
+    private val mockDtlsConnect = mockk<IDTLSConnect>()
+    private val mockCommsConfig = MockCommsConfig(mockDtlsConnect)
+    private lateinit var dtlsParameters: DTLSParameters
+    private val id1nsk = NetworkSourceKey(endpoint1)
+    private val id2nsk = NetworkSourceKey(endpoint2)
 
     init {
         every { mockDtlsConnect.init(any()) } answers {
